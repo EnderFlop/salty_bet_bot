1d9749989d023c511dd4e26bf02ea59178421d77
EnderFlop <enderflop@gmail.com>
2022-01-29 18:33:31 -0600

    Clean up
    Added chrome_options. Made headless and added descriptive console output to detail the fights as they happen. Nothing off TODO, but much cleaner and ready to start implementing logging and tournament support.

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 import math
+import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 #access https://salty.imaprettykitty.com/live/ and find the win rates for each fighter
 #use math to figure out how much to bet
@@ -16,13 +18,18 @@ PASSWORD = os.environ.get("PASSWORD")
 #Personal Credentials (for farming # of bets)
 SELF_EMAIL = os.environ.get("SELF_EMAIL")
 SELF_PASSWORD = os.environ.get("SELF_PASSWORD")
-POVERTY_LIMIT = 2000 #amount of money that, if below, always all in.
+POVERTY_LIMIT = 500 #amount of money that, if below, always all in.
+
+chrome_options = Options()
+chrome_options.add_argument("headless") #Runs Chrome without opening windows
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"]) #disables DevTools listening messages
+chrome_options.add_argument("--log-level=3") #mutes non-essential logging from chrome
 
 email, password = EMAIL, PASSWORD
 #email, password = SELF_EMAIL, SELF_PASSWORD
 
 def start_saltybet():
-  driver = webdriver.Chrome()
+  driver = webdriver.Chrome(options=chrome_options)
   
   #signin
   driver.get("https://www.saltybet.com/authenticate?signin=1") #saltybet login page
@@ -32,12 +39,28 @@ def start_saltybet():
   pass_field.send_keys(password)
   sign_in_button = driver.find_element(By.XPATH, "//input[@value='Sign In']")
   sign_in_button.click()
+  print("SIGN IN COMPLETE")
 
-  loop = 1
+  driver.implicitly_wait(9999) #wait for page to load and find balance.
+  balance = int(driver.find_element(By.XPATH, "//span[@class='dollar']").text)
+
+  loop = 0
   while True:
     driver.implicitly_wait(9999) #wait until the wager field shows up
-    wager_field = driver.find_element(By.XPATH, "//input[@style='border-color: rgb(77, 176, 68); display: inline-block;']") #wait for the wager box to show up
-    print(f"Running loop number {loop}")
+    wager_field = driver.find_element(By.XPATH, "//input[@style='border-color: rgb(77, 176, 68); display: inline-block;']") #wait for the wager box to show up/fight to be over
+
+    driver.implicitly_wait(9999) #wait to get new balance
+    past_balance = balance
+    balance = int(driver.find_element(By.XPATH, "//span[@class='dollar']").text)
+    if balance > past_balance:
+      print(f"{loop}. WON! Made ${balance - past_balance}")
+    elif balance < past_balance:
+      print(f"{loop}. LOST... Lost ${past_balance - balance}")
+
+    #START OF BETTING LOOP
+
+    loop += 1
+    print(f"{loop}. Fight Number {loop}")
 
     #get data and decide bet
     one, two = get_data()
@@ -47,14 +70,15 @@ def start_saltybet():
     #Get back the answer y (0-100). Possible points include (0,0), (5, 0.25), (10, 1), (25, 6.25), (100, 100)
     #bet y% of our current balance on the favored fighter.
     #this system is safe, since it is based on percentage difference. if two %80s go up against eachother, the engine will bet safe. We make our money off of 50% vs 80% type matches.
-    balance = int(driver.find_element(By.XPATH, "//span[@class='dollar']").text)
+
     if balance < POVERTY_LIMIT: #Since the floor is around $200, always go all in until you get out of poverty.
       to_bet = balance
+      print(f"{loop}. Odds Are {one}% vs {two}%. Current balance is ${balance}. BELOW POVERTY LIMIT! To Bet: ${to_bet}")
     else:
       percentage_bet = 0.01 * (win_percent_difference ** 2)
       #multiply the balance by the percentage (scaled by 0.01 for math). Get the floor to remove the decimal. Add 1 to always bet at least 1.
       to_bet = math.floor(balance * (percentage_bet / 100)) + 1
-    print(f"Odds Are {one}% vs {two}%. Engine gives {percentage_bet}% of balance to bet. Current balance is {balance}. To Bet: {to_bet}")
+      print(f"{loop}. Odds Are {one}% vs {two}%. Engine gives {percentage_bet}% of balance to bet. Current balance is ${balance}. To Bet: ${to_bet}")
 
     #bet
     wager_field.send_keys(to_bet)
@@ -67,16 +91,22 @@ def start_saltybet():
     #driver waits until it find the disabled wager button, then it loops and waits for the wager to reopen.
     driver.implicitly_wait(9999)
     disabled_wager = driver.find_element(By.XPATH, "//input[@style='border-color: black; display: none;']")
-    loop += 1
+    time.sleep(1) #can't use implicit wait because they already exist, just have to wait after wager closes for it to update.
+    lst = driver.find_elements(By.XPATH, "//span[@id='lastbet']/span")
+    to_gain, odds_1, odds_2 = lst[1].text, lst[2].text, lst[3].text
+    if one > two: #add some flair to the chosen fighter's odds.
+      odds_1 = f"*{odds_1}*"
+    else:
+      odds_2 = f"*{odds_2}*"
+    print(f"{loop}. Odds are {odds_1}:{odds_2}. ${to_bet} -> {to_gain} potential")
 
 def get_data():
-  data_driver = webdriver.Chrome()
+  data_driver = webdriver.Chrome(options=chrome_options)
   data_driver.get("https://salty.imaprettykitty.com/live/")
   data_driver.implicitly_wait(9999) #always wait until site loads
   winrates = data_driver.find_elements(By.XPATH, "//table[@style='margin-top: -16px; margin-bottom: 0px;']/tbody/tr[3]/td[2]")
   fighter_one = int(winrates[0].text[0:2])
   fighter_two = int(winrates[1].text[0:2])
-  print(fighter_one, fighter_two)
   # know im navigating a tightrope geting these data points, but I dont think this site is changing anytime soon. very simple html.
   data_driver.close()
   return fighter_one, fighter_two
